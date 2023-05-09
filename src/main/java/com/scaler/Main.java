37cb4f5dae4465d1b0b44d71aa55edbf8fe90e91
Rohit Saini <rohitsompalsaini@gmail.com>
2023-05-09 21:18:39 +0530

    argument passed code added to branch argument

--- src/main/java/com/scaler/Main.java
+++ src/main/java/com/scaler/Main.java
@@ -6,10 +6,19 @@ import com.scaler.http.Client;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        switch (args[0]){
+            case "http" : {
+                Client client = new Client();
+                String response = client.getResponse("https://www.google.com/");
+                System.out.println(response);
+                break;
+            }
+            case "greet" : {
+                System.out.println("Greetings");
+                break;
+            }
+            default: System.out.println("Invalid argument");
+        }
 
-        System.out.println("Hello and welcome! from Rohit");
-        Client client = new Client();
-        String response = client.getResponse("https://www.google.com/");
-        System.out.println(response);
     }
 }
